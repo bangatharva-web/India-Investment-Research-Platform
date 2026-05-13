0c8f776fdca0f16346de57c38530963b5c883f9b
Atharva Bang <atharvabang@Atharvas-MacBook-Pro.local>
2026-05-12 20:58:08 -0700

    Fix Tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     tsConfigPaths(),
     tanstackStart(),
     nitro(),
