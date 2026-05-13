32496d67887d53377f8efe9d7d63d92d47a41c9d
Atharva Bang <atharvabang@Atharvas-MacBook-Pro.local>
2026-05-12 20:12:26 -0700

    Deploy TanStack Start with Nitro on Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tanstackStart(),
-    react(),
+    nitro(),
+    viteReact(),
   ],
 });
